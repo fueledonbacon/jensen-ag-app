e2ccb687f0ab0f7b97ff04e9ab9d33d09c677095
Ryan Cwynar <ryan@fueledonbacon.com>
2020-08-05 15:33:03 +0000

    baseline entity definitions

--- index.js
+++ index.js
@@ -7,55 +7,49 @@ require('./twitter')(store)
 const typeDefs = gql`
 
   type Query{
-    latestTweet: String
-    totalTweetsReceived: Int
-    tweetsPer: Timeframes
-    top: TrendingOccurencesOf
-    percentageContaining: PercentageTweetsContaining
-  }
-  
-  type Timeframes{
-    hour: Int
-    minute: Int
-    second: Int
+    soilMoistureBalance: SoilMoistureBalanceData
   }
 
-  type TrendingOccurencesOf{
-    emojis(limit: Int): [String]
-    hashtags(limit: Int): [String]
-    domains(limit: Int): [String]
-    photos(limit: Int): [String]
+  type Grower{
+    id: ID!
+    name: String,
+    farms: [Farm]
+  }
+  type Farm{
+    id: ID!
+    fields: [Field]
+  }
+  type Field{
+    id: ID!
+    plantings: [Planting]
+  }
+  type Planting{
+    id: ID!
+    name: String
+    type: String
+    variety: String
+    plantingDate: Date
+  }
+  type Measurement{
+    id: ID!
+    type: String
+    date: Date
+    value: Number
   }
 
-  type PercentageTweetsContaining{
-    emojis: Float
-    domains: Float
-    photos: Float
+  type SoilMoistureBalanceData{
+    data: [TimeSeriesDatapoint]
+  }
+
+  type TimeSeriesDatapoint{
+    date: Date
+    value: Number
   }
 `
 
 const resolvers = {
   Query: {
-    latestTweet: (root, args, store) => store.tweets[store.currIndex].text,
-    totalTweetsReceived: (root, args, store) => store.totalTweetsReceived,
-    tweetsPer: () => ({
-      hour: (args, store) => store.tweetsPer('hour'),
-      minute: (args, store) => store.tweetsPer('minute'),
-      second: (args, store) => store.tweetsPer('second'),
-    }),
-    top: () => ({
-
-      emojis: (args, store) => store.top('emojis', args.limit),
-      hashtags: (args, store) => store.top('hashtags', args.limit),
-      domains: (args, store) => store.top('domains', args.limit),
-      photos: (args, store) => store.top('photos', args.limit),
-    }),
-    percentageContaining: () => ({
-
-      emojis: (args, store) => store.percentTweetsWith('emojis'),
-      domains: (args, store) => store.percentTweetsWith('domains'),
-      photos: (args, store) => store.percentTweetsWith('photos'),
-    }),
+    soilMoistureBalance: () => ({})
   },    
 }
 
