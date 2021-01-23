1c3a7d6657932b367e5e10d3c38c4aa0c03abb69
Ryan Cwynar <ryan@fueledonbacon.com>
2021-01-23 17:17:46 -0500

    add grower to context

--- context.js
+++ context.js
@@ -3,17 +3,19 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 const controllers = require('./controllers');
 const utilities = require('./utils')
-module.exports = async ({req}) => {
-  const jwtClaims = await utilities.getClaims(req)
-  if(jwtClaims.email){
+
+module.exports = async ({ req }) => {
+  let user = await utilities.getClaims(req)
+  if(user.email){
     const grower = await prisma.grower.findFirst({
       where : {
         email: jwtClaims.email
       }
     })
+    user.grower = grower
   }
   return {
-    jwtClaims,
+    user,
     controllers,
     prisma
   }
